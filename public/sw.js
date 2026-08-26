5470cc0b0f6bf4eb91c8eeac3817f162a863cdfc
a757k <wowwowwow12355@gmail.com>
2026-08-26 11:56:19 +0300

    Make sw.js

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "footypredictor-v1";
+
+const CORE_FILES = [
+  "/",
+  "/manifest.json",
+  "/icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CORE_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
