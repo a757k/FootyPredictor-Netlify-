b9419ac725220d76cb78e73cee0e3fb96964d338
a757k <wowwowwow12355@gmail.com>
2026-08-26 14:13:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,18 +20,6 @@ export default defineConfig({
         start_url: "/",
         scope: "/",
         orientation: "portrait",
-        icons: [
-          {
-            src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
       },
     }),
   ],
